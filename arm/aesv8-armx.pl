cebfd8b2bf6feec3a7328b713c172d5d749f5dd1
Andy Polyakov <appro@openssl.org>
2019-02-11 23:14:17 +0100

    arm/aesv8-armx.pl: authenticate return address.

--- arm/aesv8-armx.pl
+++ arm/aesv8-armx.pl
@@ -264,6 +264,7 @@ $code.=<<___;
 ${prefix}_set_decrypt_key:
 ___
 $code.=<<___	if ($flavour =~ /64/);
+	.inst	0xd503233f		// paciasp
 	stp	x29,x30,[sp,#-16]!
 	add	x29,sp,#0
 ___
@@ -307,6 +308,7 @@ $code.=<<___	if ($flavour !~ /64/);
 ___
 $code.=<<___	if ($flavour =~ /64/);
 	ldp	x29,x30,[sp],#16
+	.inst	0xd50323bf		// autiasp
 	ret
 ___
 $code.=<<___;
